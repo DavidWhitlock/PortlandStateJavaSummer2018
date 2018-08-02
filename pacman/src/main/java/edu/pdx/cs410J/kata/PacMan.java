6bb47cee680e3f9d4e6db7a3a6fe9693502a8a5e
David Whitlock <david.m.whitlock@gmail.com>
2018-08-01 19:25:32 -0700

    Renamed classes for Pac-Man game.

--- pacman/src/main/java/edu/pdx/cs410J/kata/PacMan.java
+++ pacman/src/main/java/edu/pdx/cs410J/kata/PacMan.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 /**                                                                                 
  * This class is represents a <code>Student</code>.                                 
  */                                                                                 
-public class Student extends Human {                                                
+public class PacMan extends Human {
                                                                                     
   /**                                                                               
    * Creates a new <code>Student</code>                                             
@@ -22,7 +22,7 @@ public class Student extends Human {
    * @param gender                                                                  
    *        The pacman's gender ("male" or "female", case insensitive)             
    */                                                                               
-  public Student(String name, ArrayList<String> classes, double gpa, String gender) {
+  public PacMan(String name, ArrayList<String> classes, double gpa, String gender) {
     super(name);
   }
 
--- pacman/src/test/java/edu/pdx/cs410J/kata/PacManTest.java
+++ pacman/src/test/java/edu/pdx/cs410J/kata/PacManTest.java
@@ -12,13 +12,13 @@ import static org.hamcrest.core.IsEqual.equalTo;
  * they also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>
  * matchers for more readable assertion statements.
  */
-public class StudentTest
+public class PacManTest
 {
 
   @Test
   public void studentNamedPatIsNamedPat() {
     String name = "Pat";
-    var pat = new Student(name, new ArrayList<>(), 0.0, "Doesn't matter");
+    var pat = new PacMan(name, new ArrayList<>(), 0.0, "Doesn't matter");
     assertThat(pat.getName(), equalTo(name));
   }
 

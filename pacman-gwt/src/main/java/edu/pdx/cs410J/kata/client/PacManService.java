430071384943d53e5953fcb26ce74e1d81eaafe6
David Whitlock <david.m.whitlock@gmail.com>
2018-08-08 08:00:05 -0700

    Rename servlet/service path.

--- pacman-gwt/src/main/java/edu/pdx/cs410J/kata/client/PacManService.java
+++ pacman-gwt/src/main/java/edu/pdx/cs410J/kata/client/PacManService.java
@@ -6,7 +6,7 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 /**
  * A GWT remote service that returns a dummy Phone Bill
  */
-@RemoteServiceRelativePath("phoneBill")
+@RemoteServiceRelativePath("pacman")
 public interface PacManService extends RemoteService {
 
   /**
